c1033fd5b794a43f621fddbb0a259fd0b25e9696
JavonGit <jj2874304@gmail.com>
2023-05-01 23:16:29 -0700

    init

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Javon Jack",
+    "role": "Software Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Software engineer with a proven ability to adapt in collaborative environments while staying focused on achieving high-quality results under strict deadlines. This quality and results-based approach drove me to collaborate with the VP of sales at Squarespace to create a digital marketing campaign that tripled average signups in a single month. Eager to obtain a challenging position at a prestigious company like Dream Version that will expand my learning and build upon my developer skills.",
+    "address":"Singapore",
+    "website":"https://Javon.github.io",
+    "education":[
+      {
+        "UniversityName":"National University of Singapore",
+        "specialization":" Software Engineering",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2020",
+        "Achievements":"NUS Talent Scholarship"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Appello Software",
+        "specialization":"Web Designer",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Present",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Vinova",
+        "specialization":"Software Test Engineer",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Vinova",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Angularjs"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"AWS"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"C / C++"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
